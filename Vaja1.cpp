e542a783bbbcd0a762eddffe947e75d2d18d658d
TadejSevsek <tadej.sevsek123@gmail.com>
2025-03-21 09:09:10 +0100

    step 2 of Counting Sort

--- Vaja1.cpp
+++ Vaja1.cpp
@@ -53,6 +53,12 @@ int main(int argc, const char* argv[]) {
 		for (int i = 0; i < size; i++) {
 			C[i] = 0;
 		}
+
+		
+		for (int i = 0; i < A.size(); i++) {
+			C[A[i]] = C[A[i]]+1;
+		}
+
 	}
 	else {
 		//Roman sort
